57ad45e1acd900614408ecb644400c522e9be710
Qisguys <122888167+Qisguys@users.noreply.github.com>
2025-05-09 18:12:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,8 @@ io.on('connection', (socket) => {
     io.emit('message', saved);
   });
 });
-
+app.use((req,res)=>{
+  res.send("welcome");
+})
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
